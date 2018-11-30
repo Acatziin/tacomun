9dafaf53b79fded1fc8c2f1a35c8408aaf3c3dd8
Salvador <salvador.tj@gmail.com>
2018-11-29 22:06:01 -0600

    Tablas many-to-many simples y compuestas más agregar info de usuario

--- places/models.py
+++ places/models.py
@@ -1,43 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Person(models.Model):
+class Profile(models.Model):
     #attributes definition
-    username = models.CharField(max_length=45, unique=True, null=False)
-    name = models.CharField(max_length=45)
-    lastname = models.CharField(max_length=45)
-    email = models.EmailField(max_length=100, null=False)
-    password = models.CharField(max_length=200)
-    phone = models.CharField(max_length=15)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    phone = models.CharField(max_length=12)
+    postal_code = models.IntegerField(null=False)
     birthday = models.DateField()
+
+    def __str__(self):
+        return self.user
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
 
     def __str__(self):
         return self.name
 
 class Place(models.Model):
     #attributes definition
-    place_name = models.CharField(max_length=45, unique=True, null=False)
+    name = models.CharField(max_length=45, unique=True, null=False)
     state = models.CharField(max_length=45)
     city = models.CharField(max_length=45)
     postal_code = models.IntegerField(null=False)
-    clients = models.ManyToManyField(User, through='Eval_place')
+    categories = models.ManyToManyField(Category) #Creación de relación muchos a muchos simple
+    eval = models.ManyToManyField(User, through='EvalPlace') #Relación muchos a muchos a través de una tercer tabla
 
     def __str__(self):
         return self.name
 
-class Eval_place(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+class EvalPlace(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     price = models.IntegerField(null=False)
     score = models.IntegerField(null=False)
-    comments =models.TextField()
-
-   
-
-class Category(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    category_name = models.CharField(max_length=45)
+    comment = models.TextField()
 
     def __str__(self):
-        return self.name
+        return 'score: {}'.format(self.score)
